93d90d8b0f5eb43d5105ff7aa8c87aeb49420398
Bogdan <kozitskiy.bo6dan@gmail.com>
2020-06-10 20:00:55 +0300

    deploy

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const serveStatic = require('serve-static');
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
+const app = express()
 
-const port = process.env.PORT || 5000;
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-app.listen(port);
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
