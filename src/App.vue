1ae65a4769daae8e599226bf4a4c2f615b5158fd
Bogdan <kozitskiy.bo6dan@gmail.com>
2020-06-10 22:47:54 +0300

    deploy

--- src/App.vue
+++ src/App.vue
@@ -21,4 +21,15 @@ export default {
 	@import url('./styles/reset.scss');
 	@import url('./styles/common.scss');
 	@import url('./styles/variables.scss');
+
+	.auth-wrap {
+		height: calc(100vh - 90px);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background: 
+		url('assets/images/bg-auth-2.png') bottom 20px left 20px no-repeat,
+		url('assets/images/bg-auth-1.png') top 20px right 20px no-repeat;
+		background-size: 25%;
+	}
 </style>
